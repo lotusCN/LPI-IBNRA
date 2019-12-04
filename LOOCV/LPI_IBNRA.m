86c38f5e22290a85e7567b5577d980ae99a48691
lotusCN <45529610+lotusCN@users.noreply.github.com>
2019-12-04 23:41:01 +0800

    Update LPI_IBNRA.m

--- LOOCV/LPI_IBNRA.m
+++ LOOCV/LPI_IBNRA.m
@@ -1,22 +1,22 @@
 function S_fin = LPI_IBNRA(I,AP,AL,lambda,gama ) 
 
-    %% Gaussian interaction profile kernel similarity KP,KP
+    % Gaussian interaction profile kernel similarity KP,KP
     [np,nl]=size(I);
     [KP,KL] = gaussiansimilarity(I,np,nl);  
 
-    %% integrated similarity Sim_P,Sim_L
+    % integrated similarity Sim_P,Sim_L
     Sim_P=zeros(np,np);
     Sim_P(find(AP~=0))=(AP(find(AP~=0))+KP(find(AP~=0)))/2;
     Sim_P(find(AP==0))=KP(find(AP==0));
     Sim_L=(AL+KL)/2;
 
 
-    %% integrated initial resource score S_ini
+    % integrated initial resource score S_ini
     SP=Sim_P*I.*I./sum(Sim_P,2);
     SL=I*Sim_L.*I./sum(Sim_L);   
     S_ini=(gama*SP+(1-gama)*SL)/2;
 
-    %% two-round resource allocation
+    % two-round resource allocation
     W=zeros(np,np) ;    % W:weight matrix 
     S_fin=zeros(np,nl); % S_fin:finial score matrix 
     for i=1:np  
@@ -31,7 +31,7 @@ function S_fin = LPI_IBNRA(I,AP,AL,lambda,gama )
         W(i,find(isnan(W(i,:))==1))=0;
     end
 
-    %% second-order corrlelation elimiation 
+    % second-order corrlelation elimiation 
     W=W-lambda*(W^2);  
     S_fin=W*S_ini;   
 
