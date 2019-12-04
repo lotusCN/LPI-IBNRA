af7d268755e7bd6c3c95b348b72d52820b707515
lotusCN <45529610+lotusCN@users.noreply.github.com>
2019-12-04 23:42:02 +0800

    Update plot_roc.m

--- LOOCV/plot_roc.m
+++ LOOCV/plot_roc.m
@@ -2,18 +2,10 @@
 function auc = plot_roc(deci,label_y,colour)
 [threshold,ind] = sort(deci,'descend');
 roc_y = label_y(ind);
-stack_x = cumsum(roc_y == 0)/sum(roc_y == 0);%x�᣺FPR
-stack_y = cumsum(roc_y == 1)/sum(roc_y == 1);%y�᣺TPR
+stack_x = cumsum(roc_y == 0)/sum(roc_y == 0);%xÖá£ºFPR
+stack_y = cumsum(roc_y == 1)/sum(roc_y == 1);%yÖá£ºTPR
 auc=sum((stack_x(2:length(roc_y))-stack_x(1:length(roc_y)-1)).*stack_y(2:length(roc_y)));
 
-% sn = stack_y;
-% sp = 1-stack_x;
-% 
-% N = length(label_y);
-% n = sum(label_y==0);
-% p = sum(label_y==1);
-% acc = (sn*p+sp*n)/N;
-% subplot(2,2,1);
 figure;
 plot(stack_x,stack_y,colour);
 xlabel('1-Specificity');
